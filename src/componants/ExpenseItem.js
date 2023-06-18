a8fa54c158648895659695122c8bc245cca1367f
Joy Sarkar <joysarkar8171@gmail.com>
2023-06-18 23:22:49 +0530

    onClick handeler in button added

--- src/componants/ExpenseItem.js
+++ src/componants/ExpenseItem.js
@@ -1,16 +1,25 @@
-import "./ExpenseItem.css"
-function ExpenseItem(props){
-    console.log(props.date)
-    return (
-        <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.name}</h2>
-                <div className="expense-item__price">Rs - {props.amount}</div>
-            </div>
-        </div>
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  const formatted = Intl.DateTimeFormat("en-US", {
+    day: "2-digit",
+    month: "long",
+  }).format(props.date);
+  const formattedYear = Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+  }).format(props.date);
 
-    );
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">
+        <div>{formatted}</div>
+        <div>{formattedYear}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.name}</h2>
+        <div onClick={()=>{console.log("fucked")}} className="expense-item__price">Rs - {props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
